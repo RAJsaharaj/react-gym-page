5bb92eede3fdd028b0d8bb40754541f7b580f9c1
RAJsaharaj <rajsaha03583@gmail.com>
2020-12-24 13:33:51 +0530

    add build

---
+++ build/precache-manifest.57e2dc25d8aa24dccb856bf840de83a8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3920c68623e624fc89cde1007b457d98",
+    "url": "/react-gym-page/index.html"
+  },
+  {
+    "revision": "bcd3332b96d6aeb01ecf",
+    "url": "/react-gym-page/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "65c0753a343fe7f67ee3",
+    "url": "/react-gym-page/static/css/main.8b58b3cd.chunk.css"
+  },
+  {
+    "revision": "bcd3332b96d6aeb01ecf",
+    "url": "/react-gym-page/static/js/2.bd4552d2.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react-gym-page/static/js/2.bd4552d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c0753a343fe7f67ee3",
+    "url": "/react-gym-page/static/js/main.3cc818f6.chunk.js"
+  },
+  {
+    "revision": "3f08c6a7f8f2eecb0961",
+    "url": "/react-gym-page/static/js/runtime-main.1562e5ca.js"
+  },
+  {
+    "revision": "e37644f866b574da7b60e17e2d6b876d",
+    "url": "/react-gym-page/static/media/body building.e37644f8.jpg"
+  },
+  {
+    "revision": "2266d98fd58f942f7cd5ff9bb899997b",
+    "url": "/react-gym-page/static/media/cardio training.2266d98f.jpg"
+  },
+  {
+    "revision": "7ff966f6de9bb7b9b24546c93e213293",
+    "url": "/react-gym-page/static/media/freestyle training.7ff966f6.jpg"
+  },
+  {
+    "revision": "b0f357f6308bf95b319a520fda3d8439",
+    "url": "/react-gym-page/static/media/group workout.b0f357f6.jpg"
+  },
+  {
+    "revision": "68e705614120bd1a383641518a2e4496",
+    "url": "/react-gym-page/static/media/gym1.68e70561.jpg"
+  },
+  {
+    "revision": "c689ee765bd21fb56efa282288f60ef0",
+    "url": "/react-gym-page/static/media/gym2.c689ee76.jpg"
+  },
+  {
+    "revision": "fb8fc1b4b8926821ba4503124fe261ce",
+    "url": "/react-gym-page/static/media/heavy weight classes.fb8fc1b4.jpg"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/react-gym-page/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/react-gym-page/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "13716b8f0e4002d4999d954e09c93a81",
+    "url": "/react-gym-page/static/media/strength training.13716b8f.jpg"
+  }
+]);
